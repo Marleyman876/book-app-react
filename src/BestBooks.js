f7e3b22285420018bdf489c49526e71edde75e1b
Shelby Harner <shelby.harner@gmail.com>
2021-04-28 23:11:28 -0700

    complete lab 12 card 3

---
+++ src/BestBooks.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { withAuth0 } from "@auth0/auth0-react";
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
+
+class BestBooks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const SERVER = process.env.DATABASE_URL || 'http://localhost:3001';
+      const reader = await axios.get(`${SERVER}/books`,
+        {
+          params: {
+            email: this.props.auth0.user.email
+          }
+        });
+      console.log(reader.data);
+      this.setState({ books: reader.data });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <>
+        {this.state.books.length > 0 &&
+          <Carousel>
+            {this.state.books.map((book, index) => (
+              <Carousel.Item key={index} style={{ width: '35rem' }}>
+                <Image
+                  fluid
+                  src="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+                  alt="Book"
+                />
+                <Carousel.Caption>
+                  <p>{book.name}</p>
+                  <p>{book.author}</p>
+                  <p>{book.description}</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        }
+      </>
+    )
+  }
+}
+
+export default withAuth0(BestBooks);
