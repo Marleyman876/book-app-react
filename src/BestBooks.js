7d49a5ea373f8b28cc7ed84becd577f22264fe6c
Shelby Harner <shelby.harner@gmail.com>
2021-05-01 16:12:17 -0700

    add modal, add form, add book button

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -3,13 +3,25 @@ import { withAuth0 } from "@auth0/auth0-react";
 import axios from 'axios';
 import Carousel from 'react-bootstrap/Carousel';
 import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+
+import { findAllByTestId } from '@testing-library/dom';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      modalOn: false,
     }
+  }
+
+  openModal = () => {
+    this.setState({modalOn: true});
+  }
+
+  closeModal = () => {
+    this.setState({modalOn: false});
   }
 
   async componentDidMount() {
@@ -31,26 +43,35 @@ class BestBooks extends React.Component {
   render() {
     console.log(this.state);
     return (
+
       <>
-      <h2>Best Books</h2>
-        {this.state.books.length > 0 &&
-          <Carousel>
-            {this.state.books.map((book) => (
-              <Carousel.Item key={book._id} style={{ width: '35rem' }}>
-                <Image
-                  fluid
-                  src="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-                  alt="Book"
-                />
-                <Carousel.Caption>
-                  <p>{book.name}</p>
-                  <p>{book.author}</p>
-                  <p>{book.description}</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            ))}
-          </Carousel>
-        }
+        <Button onClick={this.openModal}
+          variant="primary" 
+          size="lg">
+          Add Book
+        </Button>
+
+        <BookFormModal />
+
+        <h2>Best Books</h2>
+          {this.state.books.length > 0 &&
+            <Carousel>
+              {this.state.books.map((book) => (
+                <Carousel.Item key={book._id} style={{ width: '35rem' }}>
+                  <Image
+                    fluid
+                    src="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+                    alt="Book"
+                  />
+                  <Carousel.Caption>
+                    <p>{book.name}</p>
+                    <p>{book.author}</p>
+                    <p>{book.description}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          }
       </>
     )
   }
