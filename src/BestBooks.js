dc04a6c00560747796dd3ddd426995f5dbc317a0
Shelby Harner <shelby.harner@gmail.com>
2021-05-04 22:39:47 -0700

    work on delete function

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -6,7 +6,7 @@ import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
 import BookFormModal from './BookFormModal';
 
- import { findAllByTestId } from '@testing-library/dom';
+import { findAllByTestId } from '@testing-library/dom';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -15,8 +15,8 @@ class BestBooks extends React.Component {
       books: [],
       modalOn: false,
       name: '',
-      author:'',
-      description:''
+      author: '',
+      description: '',
     }
   };
 
@@ -28,7 +28,7 @@ class BestBooks extends React.Component {
     this.setState({ modalOn: false });
   }
   updateBooks = (books) => {
-    this.setState({books: books});
+    this.setState({ books: books });
   }
 
   async componentDidMount() {
@@ -40,53 +40,65 @@ class BestBooks extends React.Component {
             email: this.props.auth0.user.email
           }
         });
-      console.log(reader.data);
+      // console.log(reader.data);
       this.setState({ books: reader.data[0].books });
     } catch (error) {
       console.log(error);
+      alert('Error: Please refresh and try again.')
     }
   }
-  
-  render() {
-    console.log(this.state);
-    return (
 
-      <>
-        <Button onClick={this.openModal}
-          variant="primary"
-          size="lg">
-          Add Book
+  deleteBook = async (e) => {
+    const newBooks = await axios.delete(`${process.env.REACT_APP_DATABASE_URL}/books/:id`, {
+      name: this.state.name,
+      author: this.state.author,
+      description: this.state.description,
+      email: this.props.auth0.user.email
+    })
+    this.props.deleteBook(newBooks.data);
+  }
+
+render() {
+  console.log(this.state);
+  return (
+
+    <>
+      <Button onClick={this.openModal}
+        variant="primary"
+        size="lg">
+        Add Book
         </Button>
 
-        <BookFormModal
-          openModal = {this.state.modalOn}
-          closeModal = {this.closeModal}
-          newBooks = {this.handleCreateBooks}
-          updateBooks ={this.updateBooks}
-        />
+      <BookFormModal
+        openModal={this.state.modalOn}
+        closeModal={this.closeModal}
+        newBooks={this.handleCreateBooks}
+        updateBooks={this.updateBooks}
+      />
 
-        <h2>Best Books</h2>
-        {this.state.books.length > 0 &&
-          <Carousel>
-            {this.state.books.map((book) => (
-              <Carousel.Item key={book._id} style={{ width: '35rem' }}>
-                <Image
-                  fluid
-                  src="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-                  alt="Book"
-                />
-                <Carousel.Caption>
-                  <p>{book.name}</p>
-                  <p>{book.author}</p>
-                  <p>{book.description}</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            ))}
-          </Carousel>
-        }
-      </>
-    )
-  }
+      <h2>Best Books</h2>
+      {this.state.books.length > 0 &&
+        <Carousel>
+          {this.state.books.map((book) => (
+            <Carousel.Item key={book._id} style={{ width: '35rem' }}>
+              <Image
+                fluid
+                src="https://images.unsplash.com/photo-1532012197267-da84d127e765?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+                alt="Book"
+              />
+              <Carousel.Caption>
+                <p>{book.name}</p>
+                <p>{book.author}</p>
+                <p>{book.description}</p>
+                <Button onClick={() => this.deleteBook(book._id)} variant="danger" size="lg">Delete</Button>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      }
+    </>
+  )
+}
 }
 
 export default withAuth0(BestBooks);
